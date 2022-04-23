de49d7f79cde7be5a2c1c2f359deb31432c794b8
Peneloppe-jack <94473375+Peneloppe-jack@users.noreply.github.com>
2022-04-23 14:50:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -130,7 +130,7 @@ app.put('/users/:Username', passport.authenticate('jwt', { session: false }),
 
 
 //GET: list of all movies 
-app.get('/movies', passport.authenticate ('jwt', { session: false }),
+app.get('/movies', //passport.authenticate ('jwt', { session: false }),
 (req, res) => {
 Movies.find()
   .then((movies) => {
