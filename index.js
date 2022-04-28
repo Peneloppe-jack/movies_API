b6b6943c6b962fc3af42d252504b17254a7f6082
Daniela T <94473375+Peneloppe-jack@users.noreply.github.com>
2022-04-28 18:26:49 +0200

    index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(morgan('common'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 
-//const cors = require('cors');
+const cors = require('cors');
 app.use(cors());
 
 let auth = require('./auth')(app); 
