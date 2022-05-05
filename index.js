3a10f2f78570e797d5d929756109f2f4575da89f
Daniela T <94473375+Peneloppe-jack@users.noreply.github.com>
2022-05-05 13:28:33 +0200

    CORS
    allowing access

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require('cors');
 app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+   if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+     let message = 'The CORS policy for this application do not allow access from origin ' + origin;
+    return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 let auth = require('./auth')(app); 
 const passport = require('passport');
